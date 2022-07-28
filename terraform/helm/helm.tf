99751e61b603c1f79ef725c812b74c697def87f3
wantpark <wantpark@gmail.com>
2022-07-28 11:31:19 +0900

    bug fix helm.tf

--- terraform/helm/helm.tf
+++ terraform/helm/helm.tf
@@ -1,15 +1,16 @@
 provider "helm" {
  kubernetes {
+   config_path    = "~/.kube/config"
    config_context = "minikube"
  }
 }
 
 resource "helm_release" "mysql" {
  name  = "mysql"
- chart = "${abspath(path.root)}/charts/mysql-chart"
+ chart = "${abspath(path.root)}/mysql-chart"
 }
 
 resource "helm_release" "wordpress" {
  name  = "wordpress"
- chart = "${abspath(path.root)}/charts/wordpress-chart"
+ chart = "${abspath(path.root)}/wordpress-chart"
 }
